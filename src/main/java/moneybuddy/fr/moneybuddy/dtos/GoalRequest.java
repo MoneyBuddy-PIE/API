ebcdc78209c5b48369d5bd366d1eee7ce2d9a126
myrm02 <94831119+myrm02@users.noreply.github.com>
2025-12-12 16:50:54 +0100

    Reintegrated GoalRequest dtos
    Reintegration of GoalRequest dtos for goal modification endpoint validator (PUT /goals/:id)

---
+++ src/main/java/moneybuddy/fr/moneybuddy/dtos/GoalRequest.java
@@ -0,0 +1,19 @@
+package moneybuddy.fr.moneybuddy.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoalRequest {
+
+    private String name;
+
+    private Float amount;
+
+    private String emoji;
+}
