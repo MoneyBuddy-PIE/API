e5d6ce4518384707e6bc9f5d505c67a5a392790c
Ryan Ez Zerqti <ezzerqtiryan@gmail.com>
2026-01-22 11:30:28 +0100

    fix get admin chapters

--- src/main/java/moneybuddy/fr/moneybuddy/model/Chapter.java
+++ src/main/java/moneybuddy/fr/moneybuddy/model/Chapter.java
@@ -19,6 +19,7 @@ import moneybuddy.fr.moneybuddy.model.enums.SubAccountRole;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "chapters")
 @Data
@@ -32,7 +33,10 @@ public class Chapter {
   private String title;
   private String description;
   private int level;
+
+  @Field("order")
   private int order;
+
   private int coinReward;
 
   private String image_url;
--- src/main/java/moneybuddy/fr/moneybuddy/repository/ChapterRepository.java
+++ src/main/java/moneybuddy/fr/moneybuddy/repository/ChapterRepository.java
@@ -15,4 +15,7 @@ public interface ChapterRepository extends MongoRepository<Chapter, String> {
   @Query(value = "{ 'subAccountRole': ?0, 'locked': false }")
   Page<Chapter> findAllBySubAccountRoleAndLockedFalse(
       SubAccountRole subAccountRole, Pageable pageable);
+
+  @Query(value = "{}", fields = "{ 'courses' : 0 }")
+  Page<Chapter> findAllWithoutCourses(Pageable pageable);
 }
